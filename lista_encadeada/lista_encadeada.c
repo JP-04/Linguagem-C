c1e3a070ffdb01b7e3b1368f46fb19611784b817
joao Pereira <joaovitorpereiramartins.2004@gmail.com>
2022-05-05 16:22:30 -0300

    versao005

---
+++ lista_encadeada/lista_encadeada.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+//------------------------------ valor armazenado e ponteiro 
+typedef struct No{
+    int    valor_lista;
+    struct No *proximo_item
+}No;
+//------------------------------ tamanho da lista e item do topo
+typedef struct{
+    No  *inicio;
+    int tamanho_lista
+}Lista;
+//------------------------------ inserir item no topo da lista
+void inserir_topo(Lista *lista, int valor_lista){
+    No     *novo           = (No *)malloc(sizeof(No));
+    novo  -> valor_lista   = valor_lista;
+    novo  -> proximo_item  = lista->inicio;
+    lista ->inicio         = novo;
+    lista ->tamanho_lista  ++; 
+}
+//------------------------------ inserir item no fim da lista
+void inserir_final(Lista *lista, int valor_lista){
+    No    *  no  , *novo   = (No *)malloc(sizeof(No));
+    novo  -> valor_lista   = valor_lista;
+    novo  -> proximo_item  = NULL;
+
+    if( lista -> inicio == NULL){ //--- verificar se a lista esta vazia
+        lista-> inicio =  novo;      
+    }
+    else{
+        no = lista->inicio;
+        while (no->proximo_item != NULL){
+            
+            no = no->proximo_item;
+        }
+        no->proximo_item = novo;
+    }
+    lista->tamanho_lista++; 
+}
+//------------------------------ mostrar lista
+void imprimir_lista(Lista *lista){
+    No    *inicio  = lista->inicio;
+    printf("Tamanho da lista: %d \n", lista->tamanho_lista );
+    while (inicio != NULL){
+        printf("%d ", inicio->valor_lista);
+        inicio =      inicio->proximo_item;
+    }
+    printf("\n\n");
+}
+//------------------------------
+
+int main(){
+    int opcao, valor;
+
+    Lista lista;
+    lista.inicio = NULL;
+    lista.tamanho_lista = 0;    
+
+    do{
+        printf("\n------------- menu ------------- \n");
+        printf(" 1 - Inserir no inicio da lista \n 2 - Mostrar lista \n 3 - Inserir no fim da lista \n :");
+        scanf("%d", &opcao);
+
+        switch (opcao){
+    //------------------------------    
+        case 1:
+            system("cls");
+            printf("Informe um valor a ser inserido: ");
+            scanf("%d", &valor);
+            inserir_topo(&lista, valor);
+            break;
+    //------------------------------
+        case 2:
+            imprimir_lista(&lista);
+            break;
+    //------------------------------
+        case 3:
+            system("cls");
+            printf("Informe um valor a ser inserido: ");
+            scanf("%d", &valor);
+            inserir_final(&lista, valor);
+            break;
+    //------------------------------
+        default:
+            printf("Opcao invalida");
+            break;
+        }
+
+    } while (opcao != 4);    
+    return 0;
+}
